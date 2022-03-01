f61621d7384b1fc6315b6e4111f7ee4092e54c9e
Gilles Ashley <xylaray37@gmail.com>
2022-03-01 16:10:48 +0000

    added columns to hospital settings table

--- database/migrations/2022_03_01_145855_create_hospital_settings_table.php
+++ database/migrations/2022_03_01_145855_create_hospital_settings_table.php
@@ -14,8 +14,46 @@ class CreateHospitalSettingsTable extends Migration
     public function up()
     {
         Schema::create('hospital_settings', function (Blueprint $table) {
-            $table->id();
 
+            // Basic informatino
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('website')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('country')->nullable();
+            $table->string('address')->nullable();
+            $table->date('established')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('logo')->nullable();
+            $table->string('favicon')->nullable();
+            $table->string('size')->nullable();
+            $table->string('type')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('whatsapp')->nullable();
+            $table->string('instagram')->nullable();
+
+            // Mail drivers config
+            $table->string('driver')->nullable();
+            $table->string('encryption')->nullable();
+            $table->string('host')->nullable();
+            $table->string('port')->nullable();
+            $table->string('username')->nullable();
+            $table->string('email_from')->nullable();
+            $table->string('email_from_name')->nullable();
+
+            // Invoice settings config
+            $table->string('invoice_prefix')->nullable();
+            $table->string('invoice_logo')->nullable();
+            $table->string('user_prefix')->nullable();
+            $table->string('patient_prefix')->nullable();
+            $table->string('invoice_number_mood')->nullable();
+            $table->string('invoice_last_number')->nullable();
+
+            // Tax Config
+            $table->string('taxes')->default(0);
+            $table->string('discount')->default(0);
             $table->timestamps();
         });
     }
