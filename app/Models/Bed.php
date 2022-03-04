c6f2abd337f073b9dd809881ba795537e7cd5b0b
Gilles Ashley <xylaray37@gmail.com>
2022-03-04 06:25:45 +0000

    completed fillables for now

--- app/Models/Bed.php
+++ app/Models/Bed.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use App\Http\Jambas\Traits\updatableAndCreateable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bed extends Model
 {
     use HasFactory;
+    use updatableAndCreateable;
+
+    protected $fillable = [
+        'bed_no',
+        'name',
+        'price',
+        'status',
+        'image',
+        'bed_type_id',
+        'room_id',
+        'created_by_id',
+        'updated_by_id',
+    ];
 }
--- app/Models/BedType.php
+++ app/Models/BedType.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Http\Jambas\Traits\updatableAndCreateable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BedType extends Model
 {
     use HasFactory;
+    use updatableAndCreateable;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'status',
+        'created_by_id',
+        'updated_by_id',
+    ];
 }
